c40491c6857dc04a87dc957b3f5481f63119f7fc
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2023-02-16 19:11:38 +0100

    feat: add group selector

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import styles from './Header.module.css'
 import CacheStatus from './CacheStatus'
 
-const Header = ({ lastCache, setCurCourse }) => {
+const Header = ({ lastCache, setCurCourse, handleOpenGroupSel, curGroup }) => {
 
     return (
     <header className={styles.header}>
-        <div className={ styles.header__selector}><button disabled>⋮</button></div>
+        <div className={ styles.header__selector}>
+            <button title='Group selector' onClick={handleOpenGroupSel}>⋮</button>
+        </div>
         <div>
-            <h1 className={styles.header__name}>cohort21</h1>
+            <h1 className={styles.header__name}>{curGroup || ' ← Select group'}</h1>
             { false  && <CacheStatus lastCache={lastCache} setCurCourse={setCurCourse} /> }
         </div>
     </header>
