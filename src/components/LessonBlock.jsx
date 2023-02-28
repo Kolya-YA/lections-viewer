21614682d889154c0b40c184d036641b19c26b47
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2023-02-28 21:12:51 +0100

    refactor: load only some lang for highlighting

--- src/components/LessonBlock.jsx
+++ src/components/LessonBlock.jsx
@@ -1,5 +1,27 @@
-import { marked } from "marked"
-import hljs from "highlight.js"
+import { marked } from 'marked'
+import hljs from 'highlight.js/lib/core'
+
+import java from 'highlight.js/lib/languages/java'
+hljs.registerLanguage('java', java)
+
+import xml from 'highlight.js/lib/languages/xml'
+hljs.registerLanguage('xml', xml)
+
+import css from 'highlight.js/lib/languages/css'
+hljs.registerLanguage('css', css)
+
+import scss from 'highlight.js/lib/languages/scss'
+hljs.registerLanguage('scss', scss)
+
+import javascript from 'highlight.js/lib/languages/javascript'
+hljs.registerLanguage('javascript', javascript)
+
+import json from 'highlight.js/lib/languages/json'
+hljs.registerLanguage('json', json)
+
+import sql from 'highlight.js/lib/languages/sql'
+hljs.registerLanguage('sql', sql)
+
 import 'highlight.js/styles/github.css';
 import styles from './LessonPart.module.css'
 
@@ -25,8 +47,9 @@ function prepareContent(rawContent) {
             return marked(rawContent.data)
         case 'java':
             return `<pre><code>${hljs.highlight(rawContent.data, {language: 'java'}).value}</code></pre>`
+        case 'xml':
         case 'html':
-            return `<pre><code>${hljs.highlight(rawContent.data, {language: 'html'}).value}</code></pre>`
+            return `<pre><code>${hljs.highlight(rawContent.data, {language: 'xml'}).value}</code></pre>`
         case 'css':
             return `<pre><code>${hljs.highlight(rawContent.data, {language: 'css'}).value}</code></pre>`
         case 'scss':
@@ -34,7 +57,11 @@ function prepareContent(rawContent) {
         case 'js':
         case 'jsx':
             return `<pre><code>${hljs.highlight(rawContent.data, {language: 'javascript'}).value}</code></pre>`
+        case 'json':
+            return `<pre><code>${hljs.highlight(rawContent.data, {language: 'json'}).value}</code></pre>`
+        case 'sql':
+            return `<pre><code>${hljs.highlight(rawContent.data, {language: 'sql'}).value}</code></pre>`
         default:
-            return `<pre><code>${hljs.highlight(rawContent.data, {language: blockType})}</code></pre>`
+            return `<pre><code>${hljs.highlightAuto(rawContent.data)}</code></pre>`
     }
 }
