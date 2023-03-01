5afa5ee225ab69e3b2f9d59c79b01f84f77de670
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2023-03-01 21:18:07 +0100

    feat: add chatGPT generated favicon

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'lections-viewer',
+    base: '/lections-viewer',
     plugins: [react(), eslint()],
 })
