e5cdc7fec73a3b10bd2dcdce78771f1dd2ec88b6
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2023-03-01 19:56:38 +0100

    refactor: add and refactr show No of app version from package.json file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import styles from './Footer.module.css'
 const Footer = () => (
     <footer className={styles.footer}>
         <div className={styles.footer__version}>
-            v0.2.4 (react) <a href="https://github.com/Kolya-YA/lections-viewer" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://github.com/Kolya-YA/lections-viewer" target="_blank" rel="noopener noreferrer">
+                v{import.meta.env.VITE_REACT_APP_VERSION} (react) GitHub
+            </a>
         </div>
         <div className={styles.footer__copyright}>
             Copyright © WebStrom & AIT
