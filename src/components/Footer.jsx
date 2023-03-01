63c18d6ff5cff2245e8251633704f491b07c3246
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2023-03-01 12:39:26 +0100

    ver: increse version number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 const Footer = () => (
     <footer className={styles.footer}>
         <div className={styles.footer__version}>
-            v0.2 (alpha react) <a href="https://github.com/Kolya-YA/lections-viewer" target="_blank" rel="noopener noreferrer">GitHub</a>
+            v0.2.4 (react) <a href="https://github.com/Kolya-YA/lections-viewer" target="_blank" rel="noopener noreferrer">GitHub</a>
         </div>
         <div className={styles.footer__copyright}>
             Copyright © WebStrom & AIT
